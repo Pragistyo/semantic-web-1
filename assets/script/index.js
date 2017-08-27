8762303f197fed2e1c54bcc2e252914091d2bb01
ahmadaidil <ahmdaidil@gmail.com>
2017-08-27 12:00:43 +0700

    simple semantic design

---
+++ assets/script/index.js
@@ -0,0 +1,26 @@
+function about () {
+  $('.container').empty()
+  $('.container').append(`
+      <h2>About<h2>
+      <article class="">
+        <h2>Ahmad Aidil Fitri</h2>
+        <p class="lead">A random person who love to sleep when nothing he can do</p>
+      </article>
+    `)
+}
+
+function contact () {
+  $('.container').empty()
+  $('.container').append(`
+      <h2>Contact<h2>
+      <article class="">
+        <h2>Ahmad Aidil Fitri</h2>
+        <dl class="row">
+          <dt class="lead col-sm-2">Address</dt>
+          <dd class="lead col-sm-9">Cilincing, North Jakarta, Indonesia</dd>
+          <dt class="lead col-sm-2">Phone</dt>
+          <dd class="lead col-sm-9">+62 811 780 1961</dd>
+        </dl>
+      </article>
+    `)
+}
